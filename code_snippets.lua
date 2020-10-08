f841cc376b9a409b8da9c533e478d9eb27b0a64b
unknown <dusty.warns80@gmail.com>
2020-10-08 10:01:37 -0500

    init

---
+++ code_snippets.lua
@@ -0,0 +1,18 @@
+Custom widget types:
+gauge
+text
+title
+value
+ 
+Element widget types:
+antigravity_generator
+basic_control_unit
+cockpit
+core
+engine_unit
+fuel_container
+gyro
+periscope
+radar
+warpdrive
+weapon
